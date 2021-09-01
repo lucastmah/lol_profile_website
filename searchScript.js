17da67b3ff701556a5b2c8d9d3ffa163cfaafcf0
Lucas Mah <lucasmah@outlook.com>
2021-09-01 12:00:07 -0700

    Working version of player profile

--- searchScript.js
+++ searchScript.js
@@ -3,28 +3,119 @@ const serverURL = "http://54.187.199.13/test.php";
 const urlSearchParams = new URLSearchParams(window.location.search);
 const summonerName = urlSearchParams.get("summoner");
 
-var display = document.getElementById("displayName");
-var rankEmblem = document.getElementById("rankEmblem");
 var summonerIcon = document.getElementById("summonerIcon");
+var displayName = document.getElementById("summonerName");
+var soloRank = document.getElementById("soloSummonerRank");
+var soloRankEmblem = document.getElementById("soloRankEmblem");
+var soloLP = document.getElementById("soloLP");
+var soloWins = document.getElementById("soloWins");
+var soloLosses = document.getElementById("soloLosses");
+var soloWinRatio = document.getElementById("soloWinRatio");
+
+var flexRank = document.getElementById("flexSummonerRank");
+var flexRankEmblem = document.getElementById("flexRankEmblem");
+var flexLP = document.getElementById("flexLP");
+var flexWins = document.getElementById("flexWins");
+var flexLosses = document.getElementById("flexLosses");
+var flexWinRatio = document.getElementById("flexWinRatio");
+
+var winTag = document.getElementById("winTag");
+var gameOption = document.getElementById("gameOption");
+var gameMap = document.getElementById("gameMap");
+var gameLength = document.getElementById("gameLength");
+var matchPlayers = document.getElementById("matchPlayers");
+
+var playerChampImage = document.getElementById("playerChampImage");
+var champName = document.getElementById("champName");
+var summonerSpell_D = document.getElementById("summonerSpell_D");
+var summonerSpell_F = document.getElementById("summonerSpell_F");
+
+var kda = document.getElementById("kda");
+var kdaRatio = document.getElementById("kdaRatio");
+
+var playerLevel = document.getElementById("playerLevel");
+var playerCS = document.getElementById("playerCS");
+var playerKP = document.getElementById("playerKP");
 
 var response;
 
 function httpGetAsync() {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.responseType = 'json';
+    var serverRequest = new XMLHttpRequest();
+    serverRequest.responseType = 'json';
 
-    xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-            json = xmlHttp.response;
+    serverRequest.onreadystatechange = function() { 
+        if (serverRequest.readyState == 4 && serverRequest.status == 200) {
+            json = serverRequest.response;
             console.log(json);
-            rankEmblem.src = "/ranked-emblems/Emblem_" + json.soloTier + ".png";
-            summonerIcon.src = "/lol_assets/11.15.1/img/profileicon" + json.summonerIcon + ".png";
-            display.innerHTML = json.soloTier + " " + json.soloDivision;
+            summonerIcon.src = "/lol_assets/11.15.1/img/profileicon/" + json.summonerIcon + ".png";
+            displayName.innerHTML = json.summonerName;
+            //set solo ranked info
+            if(json.soloTier == "Unranked") {
+                soloRank.innerHTML = "Unranked";
+            } else {
+                soloRank.innerHTML = json.soloTier + " " + json.soloDivision;
+                soloLP.innerHTML = json.soloLP + " LP";
+                soloWins.innerHTML = json.soloWins + "W";
+                soloLosses.innerHTML = json.soloLosses + "L";
+                soloWinRatio.innerHTML = "Win Rate: " + json.soloWinRate;
+            }
+            soloRankEmblem.src = "/ranked-emblems/Emblem_" + json.soloTier + ".png";
+
+            //set flex ranked info
+            if(json.flexTier == "Unranked") {
+                flexRank.innerHTML = "Unranked";
+            } else {
+                flexRank.innerHTML = json.flexTier + " " + json.flexDivision;
+                flexLP.innerHTML = json.flexLP + " LP";
+                flexWins.innerHTML = json.flexWins + "W";
+                flexLosses.innerHTML = json.flexLosses + "L";
+                flexWinRatio.innerHTML = "Win Rate: " + json.flexWinRate;
+            }
+            flexRankEmblem.src = "/ranked-emblems/Emblem_" + json.flexTier + ".png";
+
+            //Match history
+            if(json.matches[0].matchResult) {
+                winTag.innerHTML = "Win";
+            } else {
+                winTag.innerHTML = "Loss";
+            }
+            minutes = Math.floor(json.matches[0].gameDuration / 60000);
+            seconds = Math.floor((json.matches[0].gameDuration % 60000) / 1000);
+            gameLength.innerHTML = minutes + "m " + seconds + "s";
+            gameMap.innerHTML = json.matches[0].gameMap;
+            gameOption.innerHTML = json.matches[0].gameMode.replace(' games', '');
+            json.matches[0].playerList.forEach(function (item, index) {
+                var newPlayer = document.createElement("span");
+                newPlayer.setAttribute('id', 'player' + index);
+                newPlayer.setAttribute('class', 'player');
+                matchPlayers.appendChild(newPlayer);
+                championImage = document.createElement("img");
+                championImage.src = "/lol_assets/11.15.1/img/champion/" + item.championName + ".png";
+                championImage.setAttribute('class', 'playerMiniChampionImage');
+                newPlayer.appendChild(championImage);
+                newPlayer.append(item.summonerName);
+                
+                if(item.summonerName == json.summonerName) {
+                    summonerSpell_D.src = "/lol_assets/11.15.1/img/spell/Summoner" + item.summonerD + ".png";
+                    summonerSpell_F.src = "/lol_assets/11.15.1/img/spell/Summoner" + item.summonerF + ".png";
+                    champName.innerHTML = item.championName;
+                    playerChampImage.src = "/lol_assets/11.15.1/img/champion/" + item.championName + ".png";
+                    kda.innerHTML = item.kills + " / " + item.deaths + " / " + item.assists;
+                    kdaRatio.innerHTML = ((item.kills + item.assists) / item.deaths).toFixed(2) + " : 1 KDA";
+                    playerLevel.innerHTML = "Level " + item.champLevel;
+                    playerCS.innerHTML = item.cs + " CS";
+                    if(item.teamId == "100") {
+                        playerKP.innerHTML = "KP: " + Math.round(((item.kills + item.assists) / json.matches[0].team100TotalKills) * 100) + "%";
+                    } else if(item.teamId == "200") {
+                        playerKP.innerHTML = "KP: " + Math.round(((item.kills + item.assists) / json.matches[0].team200TotalKills) * 100) + "%";
+                    }
+                }
+            });
         }
     };
 
-    xmlHttp.open("GET", serverURL + "?" + "summoner=" + summonerName, true); // true for asynchronous 
-    xmlHttp.send(null);
+    serverRequest.open("GET", serverURL + "?" + "summoner=" + summonerName, true); // true for asynchronous 
+    serverRequest.send(null);
 }
 
 httpGetAsync();
--- test.php
+++ test.php
@@ -4,13 +4,14 @@
     header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
     $NALOL = "https://na1.api.riotgames.com/lol/";
-    $apiKey = "?api_key=RGAPI-21397016-f1ca-495a-86dc-5311b4451b84";
+    $americaLOL = "https://americas.api.riotgames.com/lol/";
+    $apiKey = "api_key=RGAPI-1eb32fe4-938b-45f6-a3ce-27211d066a5b";
 
     //get ID's for given summoner
     if(isset($_GET['summoner'])) {
-        $summonerName = $_GET['summoner'];
+        $summonerInput = rawurlencode($_GET['summoner']);
 
-        $getPlayerIDS = $NALOL . "summoner/v4/summoners/by-name/" . $summonerName . $apiKey;
+        $getPlayerIDS = $NALOL . "summoner/v4/summoners/by-name/" . $summonerInput . "?" . $apiKey;
 
         $encodedPlayerIDS = file_get_contents($getPlayerIDS);
 
@@ -25,12 +26,13 @@
 
         //Get Rank
         if($playerIDS->id != null) {
+            $summonerName = $playerIDS->name;
             $encryptedSummonerID = $playerIDS->id;
             $summonerIcon = $playerIDS->profileIconId;
 
             //echo "encrypted ID: " . $encryptedSummonerID . "\n";
 
-            $getPlayerRank = $NALOL . "league/v4/entries/by-summoner/" . $encryptedSummonerID . $apiKey;
+            $getPlayerRank = $NALOL . "league/v4/entries/by-summoner/" . $encryptedSummonerID . "?" . $apiKey;
 
             $encodedPlayerRank = file_get_contents($getPlayerRank);
 
@@ -44,64 +46,81 @@
             $playerRank = json_decode($encodedPlayerRank);
             
             if($playerRank != null) {
-                if($playerRank[0]->queueType == "RANKED_SOLO_5x5") {
-                    $soloTier = ucwords(strtolower($playerRank[0]->tier));
-                    $soloDivision = $playerRank[0]->rank;
-                    $soloRank = $soloTier . " " . $soloDivision;
-                    $soloLP = $playerRank[0]->leaguePoints;
-                    $soloWins = $playerRank[0]->wins;
-                    $soloLosses = $playerRank[0]->losses;
-                    $soloWinRate = round($soloWins * 100 / ($soloWins + $soloLosses), 1) . "%";
-                    $soloPrint = $soloRank . " " . $soloLP . "LP " . $soloWinRate . " Win Rate " . $soloWins . " Wins " . $soloLosses . " Losses";
-                    if(isset($playerRank[0]->miniSeries)) {
-                        $soloPrint = $soloPrint . "\n-> Promotion: " . $playerRank[0]->miniSeries->wins . " Wins " . $playerRank[0]->miniSeries->losses . " Losses";
+                foreach($playerRank as $rank) {
+                    if($rank->queueType == "RANKED_SOLO_5x5") {
+                        $soloTier = ucwords(strtolower($rank->tier));
+                        $soloDivision = $rank->rank;
+                        $soloRank = $soloTier . " " . $soloDivision;
+                        $soloLP = $rank->leaguePoints;
+                        $soloWins = $rank->wins;
+                        $soloLosses = $rank->losses;
+                        $soloWinRate = round($soloWins * 100 / ($soloWins + $soloLosses), 1) . "%";
+                        if(isset($rank->miniSeries)) {
+                            $soloPrint = $soloPrint . "\n-> Promotion: " . $rank->miniSeries->wins . " Wins " . $rank->miniSeries->losses . " Losses";
+                        }
                     }
-                } else {
-                    $soloPrint = "Unranked";
+                    else if($rank->queueType == "RANKED_FLEX_SR") {
+                        $flexTier = ucwords(strtolower($rank->tier));
+                        $flexDivision = $rank->rank;
+                        $flexRank = $flexTier . " " . $flexDivision;
+                        $flexLP = $rank->leaguePoints;
+                        $flexWins = $rank->wins;
+                        $flexLosses = $rank->losses;
+                        $flexWinRate = round($flexWins * 100 / ($flexWins + $flexLosses), 1) . "%";
+                        if(isset($rank->miniSeries)) {
+                            $flexPrint = $flexPrint . "\n-> Promotion: " . $rank->miniSeries->wins . " Wins " . $rank->miniSeries->losses . " Losses";
+                        }
+                    } 
                 }
-
-                if($playerRank[0]->queueType == "RANKED_FLEX_SR") {
-                    $flexTier = ucwords(strtolower($playerRank[0]->tier));
-                    $flexDivision = $playerRank[0]->rank;
-                    $flexRank = $flexTier . " " . $flexDivision;
-                    $flexLP = $playerRank[0]->leaguePoints;
-                    $flexWins = $playerRank[0]->wins;
-                    $flexLosses = $playerRank[0]->losses;
-                    $flexWinRate = round($flexWins * 100 / ($flexWins + $flexLosses), 1) . "%";
-                    $flexPrint = $flexRank . " " . $flexLP . "LP " . $flexWinRate . " Win Rate " . $flexWins . " Wins " . $flexLosses . " Losses";
-                    if(isset($playerRank[0]->miniSeries)) {
-                        $flexPrint = $flexPrint . "\n-> Promotion: " . $playerRank[0]->miniSeries->wins . " Wins " . $playerRank[0]->miniSeries->losses . " Losses";
-                    }
-                } 
-                else if($playerRank[1]->queueType == "RANKED_FLEX_SR") {
-                    $flexTier = ucwords(strtolower($playerRank[1]->tier));
-                    $flexDivision = $playerRank[1]->rank;
-                    $flexRank = $flexTier . " " . $flexDivision;
-                    $flexLP = $playerRank[1]->leaguePoints;
-                    $flexWins = $playerRank[1]->wins;
-                    $flexLosses = $playerRank[1]->losses;
-                    $flexWinRate = round($flexWins * 100 / ($flexWins + $flexLosses), 1) . "%";
-                    $flexPrint = $flexRank . " " . $flexLP . "LP " . $flexWinRate . " Win Rate " . $flexWins . " Wins " . $flexLosses . " Losses";
-                    if(isset($playerRank[1]->miniSeries)) {
-                        $flexPrint = $flexPrint . "\n-> Promotion: " . $playerRank[1]->miniSeries->wins . " Wins " . $playerRank[1]->miniSeries->losses . " Losses";
-                    }
-                } else {
-                    $flexPrint = "Unranked";
-                }
-            } else {
-                $soloPrint = "Unranked";
-                $flexPrint = "Unranked";
             }
-            //echo "Solo Queue Rank: " . $soloPrint . "\n";
-            //echo "Flex Queue Rank: " . $flexPrint . "\n";
+            if($soloTier == null) {
+                $soloTier = "Unranked";
+                $soloDivision = "Unranked";
+                $soloRank = "Unranked";
+                $soloLP = "Unranked";
+                $soloWins = "Unranked";
+                $soloLosses = "Unranked";
+                $soloWinRate = "Unranked";
+            }
+            if($flexTier == null) {
+                $flexTier = "Unranked";
+                $flexDivision = "Unranked";
+                $flexRank = "Unranked";
+                $flexLP = "Unranked";
+                $flexWins = "Unranked";
+                $flexLosses = "Unranked";
+                $flexWinRate = "Unranked";
+            }
+
         } else {
             return http_response_code(404);
         }
 
-        
+        //Get queues into a php variable
+        $encodedQueueTypes = file_get_contents("./queues.json");
+
+        if(is_null($encodedQueueTypes)) {
+            return http_response_code(404);
+        }
+
+        $queueTypes = json_decode($encodedQueueTypes);
+
+        //Get summoners into a php variable
+        $encodedSummoners = file_get_contents("./summoner.json");
+
+        if(is_null($encodedSummoners)) {
+            return http_response_code(404);
+        }
+
+        $summoners = json_decode($encodedSummoners);
+
+
         //Get Match History
         if($playerIDS->accountId != null) {
-            $getPlayerMatchHistory = $NALOL . "match/v4/matchlists/by-account/" . $playerIDS->accountId . $apiKey;
+            $encryptedSummonerPUUID = $playerIDS->puuid;
+            $startMatchIndex = 0;
+            $endMatchIndex = 20;
+            $getPlayerMatchHistory = $americaLOL . "match/v5/matches/by-puuid/" . $encryptedSummonerPUUID . "/ids?start=" . $startMatchIndex . "&count=" . $endMatchIndex ."&" . $apiKey;
 
             $encodedPlayerMatchHistory = file_get_contents($getPlayerMatchHistory);
 
@@ -112,8 +131,84 @@
             $playerMatchHistory = json_decode($encodedPlayerMatchHistory);
 
             if($playerMatchHistory != null) {
-                if($playerMatchHistory->matches != null) {
-                    $matchHistory = $playerMatchHistory->matches;
+                $matchHistory = array();
+                foreach($playerMatchHistory as $match) {
+                    $getPlayerSingleMatch = $americaLOL . "match/v5/matches/" . $match . "?" . $apiKey;
+
+                    $encodedPlayerSingleMatch = file_get_contents($getPlayerSingleMatch);
+
+                    if(is_null($encodedPlayerSingleMatch)) {
+                        return http_response_code(404);
+                    }
+
+                    $playerSingleMatch = json_decode($encodedPlayerSingleMatch);
+
+                    //Get individual player information for a match
+                    $playerList = array();
+                    foreach($playerSingleMatch->info->participants as $participant) {
+                        foreach($summoners->data as $summoner) {
+                            if($summoner->key == $participant->summoner1Id) {
+                                $summonerD = $summoner->name;
+                            }
+                            if($summoner->key == $participant->summoner2Id) {
+                                $summonerF = $summoner->name;
+                            }
+                        }
+                        $player = array(
+                            'summonerName' => $participant->summonerName,
+                            'championId' => $participant->championId,
+                            'championName' => $participant->championName,
+                            'kills' => $participant->kills,
+                            'deaths' => $participant->deaths,
+                            'assists' => $participant->assists,
+                            'position' => $participant->individualPosition,
+                            'champLevel' => $participant->champLevel,
+                            'summonerD' => $summonerD,
+                            'summonerF' => $summonerF,
+                            'cs' => $participant->totalMinionsKilled,
+                            'teamId' => $participant->teamId
+                        );
+
+                        if($participant->summonerName == $summonerName) {
+                            $gameResult = $participant->win;
+                        }
+
+                        array_push($playerList, $player);
+                    }
+
+                    //find queue type
+                    foreach($queueTypes as $queueType) {
+                        if($queueType->queueId == $playerSingleMatch->info->queueId) {
+                            $gameMap = $queueType->map;
+                            $gameMode = $queueType->description;
+                        }
+                    }
+
+                    //Get total kills for each side
+                    foreach($playerSingleMatch->info->teams as $teamStats) {
+                        if($teamStats->teamId == "100") {
+                            $team100TotalKills = $teamStats->objectives->champion->kills;
+                        }
+                        if($teamStats->teamId == "200") {
+                            $team200TotalKills = $teamStats->objectives->champion->kills;
+                        }
+                    }
+
+                    //Set data for a single match
+                    $returnMatch = array (
+                        'matchId' => $playerSingleMatch->metadata->matchId,
+                        'gameMap' => $gameMap,
+                        'gameMode' => $gameMode,
+                        'gameVersion' => $playerSingleMatch->info->gameVersion,
+                        'mapId' => $playerSingleMatch->info->mapId,
+                        'gameDuration' => $playerSingleMatch->info->gameDuration,
+                        'gameResult' => $gameResult,
+                        'team100TotalKills' => $team100TotalKills,
+                        'team200TotalKills' => $team200TotalKills,
+                        'playerList' => $playerList
+                    );
+
+                    array_push($matchHistory, $returnMatch);
                 }
             }
         }
@@ -121,19 +216,24 @@
         return http_response_code(404);
     }
 
+    //JSON data
     $file = array(
         'summonerName' => $summonerName,
         'summonerIcon' => $summonerIcon,
+        'summonerID' => $encryptedSummonerID,
+        'summonerPUUID' => $encryptedSummonerPUUID,
         'soloLP' => $soloLP,
         'soloTier' => $soloTier,
         'soloDivision' => $soloDivision,
         'soloWins' => $soloWins,
         'soloLosses' => $soloLosses,
+        'soloWinRate' => $soloWinRate,
         'flexTier' => $flexTier,
         'flexDivision' => $flexDivision,
         'flexLP' => $flexLP,
         'flexWins' => $flexWins,
         'flexLosses' => $flexLosses,
+        'flexWinRate' => $flexWinRate,
         'matches' => $matchHistory
     );
 
